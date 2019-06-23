509548a8d2c8ce4ffb7fb872f8bb79f75a1401db
Syarif Nur <syarif.nur@student.spb.ac.id>
2019-06-23 18:47:43 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
-# Write your #display_rainbow method here
+warna = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow(warna)
+  puts "R: #{warna[0]}, O: #{warna[1]}, Y: #{warna[2]}, G: #{warna[3]}, B: #{warna[4]}, I: #{warna[5]}, V: #{warna[6]}"
+end

afc912ec6f18df1308a7d1b299a65fdbad48566f
Syarif Nur <syarif.nur@student.spb.ac.id>
2019-06-23 18:46:04 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 warna = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(warna)
-  puts "R: #{warna[0]}"
+  puts "R: #{warna[0]}, Y: #{warna[1]}, G: #{warna[2]}, B: #{warna[3]}, I: #{warna[4]}, V: #{warna[5]}"
 end

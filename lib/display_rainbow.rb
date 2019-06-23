105a37df913831d36d95ffdcdae1866cd75cedc3
Syarif Nur <syarif.nur@student.spb.ac.id>
2019-06-23 18:37:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 bob = ["Bob", 46, "Juggler", "555-555-5555"]
 
 def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[3]}"
-  puts "Contact: #{person[4]}"
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
 end

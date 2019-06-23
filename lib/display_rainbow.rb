3b0fdde9f4d993fd8d15e8bd7650a1973d22ca0d
Syarif Nur <syarif.nur@student.spb.ac.id>
2019-06-23 18:43:14 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,5 @@
-display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+warna = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow(display_rainbow)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
+def display_rainbow(warna)
+  puts "Name: #{warna[0]}, Age: #{warna[1]}, Occupation: #{warna[2]}"
 end

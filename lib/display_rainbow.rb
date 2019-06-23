fa2fe58c2a6139d71908bfba8be90e9b5ebe535c
Syarif Nur <syarif.nur@student.spb.ac.id>
2019-06-23 18:42:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow(person)
+def display_rainbow(display_rainbow)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
